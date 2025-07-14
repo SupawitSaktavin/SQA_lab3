4a6e3e117b4517906168c1938400e232223518ba
Supawit Saktavin <supawit.sa@kkumail.com>
2025-07-14 23:02:22 +0700

    update test negative 1 again

--- RomanNumerals/src/Test/test_negative1.java
+++ RomanNumerals/src/Test/test_negative1.java
@@ -10,32 +10,20 @@ class test_negative1 {
 
 	@Test
 	void TC_40() {
-		try {
-	        test.convertRomanNumToInt("ABC");
-	        fail("Expected exception for invalid input 'ABC'");
-	    } catch (NullPointerException  e) {
-
-	    }
+		test.convertRomanNumToInt("ABC");
+		fail("Expected an exception for invalid input 'ABC");
 	}
 	
 	@Test
 	void TC_41() {
-		try {
-	        test.convertRomanNumToInt("J");
-	        fail("Expected exception for invalid input 'J'");
-	    } catch (NullPointerException  e) {
-
-	    }
+		test.convertRomanNumToInt("J");
+		fail("Expected an exception for invalid input 'J");
 	}
 	
 	@Test
 	void TC_42() {
-		try {
-	        test.convertRomanNumToInt("XZ");
-	        fail("Expected exception for invalid input 'XZ'");
-	    } catch (NullPointerException  e) {
-
-	    }
+		test.convertRomanNumToInt("XZ");
+		fail("Expected an exception for invalid input 'X");
 	}
 
 }
